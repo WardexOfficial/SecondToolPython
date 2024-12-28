f4c0204290d0bb62491cf1f934a59550fb4f9300
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2024-12-29 00:37:03 +0300

    load source

---
+++ bin/core.py
@@ -0,0 +1,39 @@
+from colorama import Fore
+from bin import config
+import time, os, configparser, sys
+
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
+
+def start():
+    print(Fore.YELLOW + config.LOGO)
+    print('SecondTool presents' + Fore.RESET)
+    try: 
+        a = int(input(Fore.CYAN + f'''
+1.DDOS
+2.SEARCH
+3.HELP
+9.RESTART {Fore.YELLOW}[DEV]{Fore.CYAN}
+0.QUIT
+>>> ''' + Fore.YELLOW))
+    except ValueError:
+        print(Fore.RED + 'FATAL ERROR' + Fore.RESET)
+        time.sleep(3)
+        clear()
+        start()
+    
+    if a == 0:
+        exit(1)
+    elif a == 9:
+        python_exec = sys.executable
+        os.execl(python_exec, python_exec, *sys.argv)
+    
+    if a == 1:
+        ...
+    elif a == 2:
+        ...
+    elif a == 3:
+        print(Fore.CYAN + '''
+Help? You need help? Are you crazy?
+        ''' + Fore.RESET)
+    
+    exit(1)
---
+++ main.py
@@ -0,0 +1,81 @@
+import subprocess, sys, os, time, configparser
+from bin import config
+
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
+    
+def install_dependencies():
+    try:
+        with open("requirements.txt") as f:
+            dependencies = [line.strip() for line in f if line.strip()]
+        installed = subprocess.check_output([sys.executable, "-m", "pip", "freeze"]).decode('utf-8')
+        for dependency in dependencies:
+            if dependency.split("==")[0].strip().lower() not in installed.lower():
+                subprocess.check_call([sys.executable, "-m", "pip", "install", dependency])
+                print(f"Зависимость '{dependency}' установлена.")
+            else:
+                print(f"Зависимость '{dependency}' уже установлена.")
+    except FileNotFoundError:
+        print("Файл requirements.txt не найден.")
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при установке: {e}")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+if config.CHECK_PIP_LIBS is True:
+    install_dependencies()
+
+from colorama import Fore, init
+from git import Repo
+init()
+print(Fore.GREEN + 'Иницилизация ядра...' + Fore.RESET)
+time.sleep(1)
+clear()
+
+cnf = configparser.ConfigParser()
+cnf.read('config.ini')
+check_update = cnf["into"]["check_update"].lower() == 'true'
+use_git = cnf["into"]["use_git"].lower() == 'true'
+
+if check_update is True:
+    if use_git is True:
+        try:
+            repo_path = os.path.dirname(os.path.abspath(__file__))
+            if not os.path.isdir(os.path.join(repo_path, '.git')):
+                print(Fore.RED + f"Ошибка: директория {repo_path} не является репозиторием!" + Fore.RESET)
+                exit(401)
+
+            repo = Repo(repo_path)
+            origin = repo.remote(name='origin')
+            origin.fetch()
+
+            current_branch = repo.active_branch.name
+            remote_ref = f'origin/{current_branch}'
+
+            local_commit = repo.commit(current_branch)
+            remote_commit = repo.commit(remote_ref)
+
+            #if repo.is_dirty(untracked_files=True):
+            #    print(Fore.RED + "Есть несохранённые изменения. Пожалуйста, закоммитьте их или удалите." + Fore.RESET)
+            #    exit()
+
+            if local_commit != remote_commit:
+                print("Выполняется обновление...")
+                origin.pull()
+                print("Обновление завершено. Скрипт перезапускается...")
+                time.sleep(0.1)
+                python_exec = sys.executable
+                os.execl(python_exec, python_exec, *sys.argv)
+            else:
+                print(Fore.GREEN + "Новых обновлений не обнаружено" + Fore.RESET)
+                time.sleep(1)
+        except Exception as e:
+            print(f"Ошибка при обновлении: {e}")
+    else:
+        print(Fore.RED + 'Поддержка git отключена. Для обновления включите поддержку git либо отключите проверку обновлений. Для этого измените настройки в файле config.ini' + Fore.RESET)
+        exit(401)
+else:
+    print(Fore.RED + 'Проверка обновлений отключена' + Fore.RESET)
+
+from bin import core
+core.start()
+exit()
