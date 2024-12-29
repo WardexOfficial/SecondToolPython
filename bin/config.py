74a495b1be0fd4c2999c8b87d0bf5cdd2e08b0ba
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2024-12-29 18:00:30 +0300

    new commit

--- bin/config.py
+++ bin/config.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 LOGO = '''
 ███████╗███████╗ ██████╗ ██████╗ ███╗   ██╗██████╗
 ██╔════╝██╔════╝██╔════╝██╔═══██╗████╗  ██║██╔══██╗
@@ -6,3 +8,18 @@ LOGO = '''
 ███████║███████╗╚██████╗╚██████╔╝██║ ╚████║██████╔╝
 ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝'''
 CHECK_PIP_LIBS = True
+DB_FILE = 'data.db'
+LIBS_FOLDER = 'libs'
+
+def init_database():
+    sql = sqlite3.connect(DB_FILE)
+    cursor = sql.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS libs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    version TEXT
+    )""")
+    sql.commit()
+
+    return sql, cursor
