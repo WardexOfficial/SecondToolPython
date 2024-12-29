34d3029291c70688a1c286a9e70d7d660cc40f5b
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2024-12-29 18:04:11 +0300

    new commit 1

--- bin/core.py
+++ bin/core.py
@@ -6,44 +6,14 @@ def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
 def start():
     clear()
-
-    if config.LIBS_FOLDER not in sys.path:
-        sys.path.append(config.LIBS_FOLDER)
-    
-    sql, cursor = config.init_database()
-    cursor.execute("SELECT * FROM libs")
-    libs = cursor.fetchall()
-    commands_result = {}
-
-    for lib in libs:
-        lib_name = lib[1]
-        lib_path = os.path.join(config.LIBS_FOLDER, f"{lib_name}.py")
-        try:
-            if not os.path.exists(lib_path):
-                print(f"Файл {lib_path} для модуля {lib_name} не найден.")
-                continue
-            
-            spec = importlib.util.spec_from_file_location(lib_name, lib_path)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            
-            if hasattr(module, "commands") and callable(module.commands):
-                commands_result[lib_name] = module.commands()
-            else:
-                print(f"Модуль {lib_name} не содержит функцию 'commands'.")
-        except Exception as e:
-            print(f"Ошибка при обработке модуля {lib_name}: {e}")
-    
-    for lib_name, commands in commands_result.items():
-        print(f"Команды из {lib_name}: {commands}")
-
     print(Fore.YELLOW + config.LOGO)
     print('SecondTool presents' + Fore.RESET)
     try: 
         a = int(input(Fore.CYAN + f'''
 1.DDOS
 2.SEARCH
-3.HELP
+3.MODULE`s
+4.HELP
 9.RESTART {Fore.YELLOW}[DEV]{Fore.CYAN}
 0.QUIT
 >>> ''' + Fore.YELLOW))
@@ -64,6 +34,36 @@ def start():
     elif a == 2:
         ...
     elif a == 3:
+        if config.LIBS_FOLDER not in sys.path:
+            sys.path.append(config.LIBS_FOLDER)
+
+        sql, cursor = config.init_database()
+        cursor.execute("SELECT * FROM libs")
+        libs = cursor.fetchall()
+        commands_result = {}
+
+        for lib in libs:
+            lib_name = lib[1]
+            lib_path = os.path.join(config.LIBS_FOLDER, f"{lib_name}.py")
+            try:
+                if not os.path.exists(lib_path):
+                    print(f"Файл {lib_path} для модуля {lib_name} не найден.")
+                    continue
+                
+                spec = importlib.util.spec_from_file_location(lib_name, lib_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                
+                if hasattr(module, "commands") and callable(module.commands):
+                    commands_result[lib_name] = module.commands()
+                else:
+                    print(f"Модуль {lib_name} не содержит функцию 'commands'.")
+            except Exception as e:
+                print(f"Ошибка при обработке модуля {lib_name}: {e}")
+        
+        for lib_name, commands in commands_result.items():
+            print(f"Команды из {lib_name}: {commands}")
+    elif a == 4:
         print(Fore.CYAN + '''
 Help? You need help? Are you crazy?
         ''' + Fore.RESET)
