db32e7d79731b8a990be6fae08c0e3c6f54fd969
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2024-12-29 18:58:47 +0300

    add edit cnf file with base and functions

--- libs/base.py
+++ libs/base.py
@@ -1,11 +1,32 @@
 from colorama import Fore
 from bin.core import restart
-import time, shutil, os
+import time, shutil, os, configparser
+
+cnf = configparser.ConfigParser()
+cnf.read('config.ini')
 
 def commands():
     return { 
+        'enable --checkupdate': enable_check_update,
+        'disable --checkupdate': disable_check_update,
         'clearcache': clearcache
     }
+
+def enable_check_update():
+    cnf.set('into', 'check_update', 'true')
+    with open('config.ini', 'w') as configfile:
+        cnf.write(configfile)
+    print(Fore.GREEN + 'Проверка обновлений включена!' + Fore.RESET)
+    time.sleep(2)
+    return restart()
+
+def disable_check_update():
+    cnf.set('into', 'check_update', 'false')
+    with open('config.ini', 'w') as configfile:
+        cnf.write(configfile)
+    print(Fore.GREEN + 'Проверка обновлений отключена!' + Fore.RESET)
+    time.sleep(2)
+    return restart()
 
 def clearcache(root_dir="."):
     for dirpath, dirnames, filenames in os.walk(root_dir):
