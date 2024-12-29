92901a11ab9435dd611fe3ce3a23be69733a607d
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2024-12-29 18:27:11 +0300

    update base module

--- libs/base.py
+++ libs/base.py
@@ -1,13 +1,19 @@
 from colorama import Fore
 from bin.core import start
-import time
+import time, shutil, os
 
 def commands():
     return { 
         'clearcache': clearcache
     }
 
-def clearcache():
+def clearcache(root_dir="."):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for dirname in dirnames:
+            if dirname == "__pycache__":
+                pycache_path = os.path.join(dirpath, dirname)
+                shutil.rmtree(pycache_path)
+                print(f"Удалена папка: {pycache_path}")
     print(Fore.GREEN + 'Cache cleared!' + Fore.RESET)
     time.sleep(1)
     return start()
